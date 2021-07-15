a48fd16cb121bf077cbaf9141eb45eda3f866fef
elenaprieto27 <elenaprieto.c59@gmail.com>
2021-07-14 22:10:56 -0500

    prueba welcome

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [\App\Http\Controllers\Controller::class, 'matcha']);
+//Route::get('/', [\App\Http\Controllers\Controller::class, 'matcha']);
+
+Route::get('/', function(){
+    return view('welcome');
+});
 
