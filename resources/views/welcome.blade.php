53c2e4e855abb8e114aa102b0a27c70070e9bae6
elenaprieto27 <elenaprieto.c59@gmail.com>
2021-07-15 00:22:17 -0500

    Cambio 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Ele 1
                 </div>
 
                 <div class="links">
--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,11 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+Route::get('/', [\App\Http\Controllers\Controller::class, 'matcha']);
