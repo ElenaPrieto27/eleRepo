063f9cac9a02ca9bb54cebe22dc2c5358c2d5bf3
elenaprieto27 <elenaprieto.c59@gmail.com>
2021-07-15 16:58:34 -0500

    Request i

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 use App\elena2;
