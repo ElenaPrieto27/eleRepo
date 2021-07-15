19126923426220ade3dac8e439439191da20b32c
elenaprieto27 <elenaprieto.c59@gmail.com>
2021-07-15 01:26:14 -0500

    Ele 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Elena Prieto
+                Laravel Ele 2
                 </div>
 
                 <div class="links">
