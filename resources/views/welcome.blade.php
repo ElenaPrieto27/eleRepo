34d922598a0d07a74da63481c837a6c200907d77
elenaprieto27 <elenaprieto.c59@gmail.com>
2021-07-26 18:24:27 -0500

    Act del composer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Elena Prieto 2
+                Elena Prieto 
                 </div>
 
                 <div class="links">
