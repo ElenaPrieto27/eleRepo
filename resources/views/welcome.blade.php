68a043e1ab5bb4148c790afbcd6c4a3868dec6aa
elenaprieto27 <elenaprieto.c59@gmail.com>
2021-07-26 21:53:22 -0500

    Ele 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Elena Prieto 1
+                Elena Prieto 2
                 </div>
 
                 <div class="links">
